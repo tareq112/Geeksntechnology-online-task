86cef31492c44115506dac437393c7a12d9a0210
Mollah Tareq <tareqcseuiu@gmail.com>
2021-10-18 16:25:05 +0600

    html & css js

--- jscript.js
+++ jscript.js
@@ -1,27 +1,28 @@
 const singnInBtn = document.querySelector("#show-login");
 const closeBtnpoppup = document.querySelector(".popup .close-btn");
 const sigInDashboard = document.querySelector('#sigInDashboard');
-const tablehide = document.querySelector('#hiddentable');
+const tablehide = document.querySelector('.container #hiddentable');
 
 //sign in pop up close 
-singnInBtn.addEventListener("click", function () {
+singnInBtn.addEventListener("click", () => {
   const popupactive = document.querySelector(".popup");
   popupactive.classList.add("active");
 });
 
-closeBtnpoppup.addEventListener("click", function () {
-  const popupRemove = document.querySelector(".popup");
-     popupRemove.classList.remove("active");
-});
+closeBtnpoppup.addEventListener("click", () => {
+ const popupRemove = document.querySelector(".popup");
+   popupRemove.classList.remove("active");
+ });
 
 //click sign in button for go to dashboard page
 
-sigInDashboard.addEventListener('click', function () {
+sigInDashboard.addEventListener('click', () => {
   console.log('hello');
   location.href = "dashboard.html";
 })
 
 //customer list table  
-tablehide.addEventListener('click', function () {
-  console.log('hello');
-})
+tablehide.addEventListener('click', () => {
+    console.log('hello');
+  });
+
