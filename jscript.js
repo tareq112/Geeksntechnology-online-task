95aaf8aba14c2a13e2e3080173f13b6703a88a8f
Mollah Tareq <tareqcseuiu@gmail.com>
2021-10-19 23:34:43 +0600

    html & css js

--- jscript.js
+++ jscript.js
@@ -1,28 +1,11 @@
-const singnInBtn = document.querySelector("#show-login");
-const closeBtnpoppup = document.querySelector(".popup .close-btn");
-const sigInDashboard = document.querySelector('#sigInDashboard');
-const tablehide = document.querySelector('.container #hiddentable');
+const tablehide = document.querySelector('.customerList');
+tablehide.addEventListener('click', (e) => {
+    console.log(e);
 
-//sign in pop up close 
-singnInBtn.addEventListener("click", () => {
-  const popupactive = document.querySelector(".popup");
-  popupactive.classList.add("active");
+    const tableDisplay = document.querySelector('#hiddentable');
+   // tableDisplay.addEventListener
+       tableDisplay.style.display = " " ? 'none' : 'block';
+    
 });
 
-closeBtnpoppup.addEventListener("click", () => {
- const popupRemove = document.querySelector(".popup");
-   popupRemove.classList.remove("active");
- });
-
-//click sign in button for go to dashboard page
-
-sigInDashboard.addEventListener('click', () => {
-  console.log('hello');
-  location.href = "dashboard.html";
-})
-
-//customer list table  
-tablehide.addEventListener('click', () => {
-    console.log('hello');
-  });
 
